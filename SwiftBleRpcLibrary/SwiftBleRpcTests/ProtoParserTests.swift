25f38df5c9b1d476ed509a0828d8769be07d6a69
Vadim K <v.korepanov@auraband.io>
2021-06-17 16:36:08 +0500

    Fix test

--- SwiftBleRpcLibrary/SwiftBleRpcTests/ProtoParserTests.swift
+++ SwiftBleRpcLibrary/SwiftBleRpcTests/ProtoParserTests.swift
@@ -60,4 +60,61 @@ class ProtoParserTests: XCTestCase {
     func testDecodeWrongType() {
         expect { try ProtoDecoder.decode(data: Data.init(hex: "7BAAAAAA"), from: 0, to: 4, type: .unknown) as? Int32 }.to(throwError(ProtoParserErrors.notSupportedType))
     }
+
+    func testEncodeInt1Byte() throws {
+        let encoded = try ProtoEncoder.encode(object: 123, from: 0, to: 1, type: .int32)
+        expect(encoded).to(equal(Data.init(hex: "7B")))
+    }
+
+    func testEncodeInt2Byte() throws {
+        let encoded = try ProtoEncoder.encode(object: 35278, from: 0, to: 2, type: .int32)
+        expect(encoded).to(equal(Data.init(bytes: [206, 137])))
+    }
+
+    func testEncodeIntMoreThanFourBytesFails() throws {
+        expect { try ProtoEncoder.encode(object: 35278, from: 0, to: 5, type: .int32) }.to(throwError(ProtoParserErrors.wrongData))
+    }
+
+    func testEncodeBoolTrue() throws {
+        let encoded = try ProtoEncoder.encode(object: true, from: 0, to: 1, type: .bool)
+        expect(encoded).to(equal(Data.init(bytes: [1])))
+    }
+
+    func testEncodeBoolFalse() throws {
+        let encoded = try ProtoEncoder.encode(object: false, from: 0, to: 1, type: .bool)
+        expect(encoded).to(equal(Data.init(bytes: [0])))
+    }
+
+    func testEncodeBoolMoreThanOneBytesFails() throws {
+        expect { try ProtoEncoder.encode(object: true, from: 0, to: 2, type: .bool) }.to(throwError(ProtoParserErrors.wrongData))
+    }
+
+    func testEncodeEmptyBytes() throws {
+        let encoded = try ProtoEncoder.encode(object: Data(), from: 0, to: 0, type: .byte)
+        expect(encoded).to(equal(Data()))
+    }
+
+    func testEncodePartBytes() throws {
+        expect { try ProtoEncoder.encode(object: Data(hex: "36172540"), from: 0, to: 1, type: .byte) }.to(throwError(ProtoParserErrors.wrongData))
+    }
+
+    func testEncodeFullBytes() throws {
+        let data = Data.init(hex: "36172540")
+        let encoded = try ProtoEncoder.encode(object: data, from: 0, to: 4, type: .byte)
+        expect(encoded).to(equal(data))
+    }
+
+    func testEncodeFromNotZeroBytes() throws {
+        let data = Data.init(hex: "2540")
+        let encoded = try ProtoEncoder.encode(object: data, from: 2, to: 4, type: .byte)
+        expect(encoded).to(equal(Data.init(hex: "2540")))
+    }
+
+    func testEncodeBytesWrongSize() throws {
+        expect { try ProtoEncoder.encode(object: Data(hex: "36172540"), from: 0, to: 15, type: .byte) }.to(throwError(ProtoParserErrors.wrongData))
+    }
+
+    func testEncodeWrongType() {
+        expect { try ProtoEncoder.encode(object: 123, from: 0, to: 4, type: .unknown) }.to(throwError(ProtoParserErrors.notSupportedType))
+    }
 }
